03babec4c42499c3cccec9fa7064cf72969f6036
Dante Solorio <contact@deipi.com>
2017-02-03 19:09:17 -0600

    Ep 10 add stack view for bottom buttons

--- TwitterApp/TweetCell.swift
+++ TwitterApp/TweetCell.swift
@@ -31,6 +31,7 @@ class TweetCell: DatasourceCell {
     
     let messageTextView: UITextView = {
         let tv = UITextView()
+        tv.backgroundColor = .clear
         tv.text = "SOME SAMPLE TEXT"
         tv.isEditable = false
         return tv
@@ -44,6 +45,30 @@ class TweetCell: DatasourceCell {
         return iv
     }()
     
+    let replyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "reply").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let retweetButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "retweet").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let likeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "like").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
+    let directMessageButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "send_message").withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
     override func setupViews() {
         super.setupViews()
         
@@ -54,10 +79,43 @@ class TweetCell: DatasourceCell {
         
         addSubview(profileImageView)
         addSubview(messageTextView)
+        addSubview(replyButton)
+        addSubview(retweetButton)
         
         profileImageView.anchor(topAnchor, left: leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 50, heightConstant: 50)
         
         messageTextView.anchor(topAnchor, left: profileImageView.rightAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 4, leftConstant: 4, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
-
+    
+        setupBottomButtons()
+    }
+    
+    fileprivate func setupBottomButtons(){
+        let replyButtonContainer = UIView()
+        
+        let retweetButtonContainer = UIView()
+        
+        let likeButtonContainer = UIView()
+        
+        let directMessageButtonContainer = UIView()
+        
+        let buttonStackView = UIStackView(arrangedSubviews: [replyButtonContainer, retweetButtonContainer, likeButtonContainer, directMessageButtonContainer])
+        buttonStackView.axis = .horizontal
+        buttonStackView.distribution = .fillEqually
+        
+        addSubview(buttonStackView)
+        buttonStackView.anchor(nil, left: messageTextView.leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 4, rightConstant: 0, widthConstant: 0, heightConstant: 20)
+        
+        addSubview(replyButton)
+        addSubview(retweetButton)
+        addSubview(likeButton)
+        addSubview(directMessageButton)
+        
+        replyButton.anchor(replyButtonContainer.topAnchor, left: replyButtonContainer.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        
+        retweetButton.anchor(retweetButtonContainer.topAnchor, left: retweetButtonContainer.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        
+        likeButton.anchor(likeButtonContainer.topAnchor, left: likeButtonContainer.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
+        
+        directMessageButton.anchor(directMessageButtonContainer.topAnchor, left: directMessageButtonContainer.leftAnchor, bottom: nil, right: nil, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 20, heightConstant: 20)
     }
 }
