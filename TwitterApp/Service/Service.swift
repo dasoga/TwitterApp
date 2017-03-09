6c61d63c31f59d1abff36000765519367e47a3d4
Dante Solorio <ddasogaa@gmail.com>
2017-03-09 13:23:25 -0600

    Ep 12 Singleton and completion

---
+++ TwitterApp/Service/Service.swift
@@ -0,0 +1,39 @@
+//
+//  Service.swift
+//  TwitterApp
+//
+//  Created by Dante Solorio on 3/9/17.
+//  Copyright © 2017 iOS México School. All rights reserved.
+//
+
+import Foundation
+import TRON
+import SwiftyJSON
+
+struct Service {
+    
+    let tron = TRON(baseURL: "https://api.letsbuildthatapp.com")
+    
+    static let sharedInstance = Service()
+    
+    
+    func fetchHomeFeed(completion: @escaping (HomeDataSource) -> ()) {
+        print("Fetching home feed")
+        
+        // Start JSON fetch
+        let request: APIRequest<HomeDataSource, JSONError> = tron.request("/twitter/home")
+        
+        request.perform(withSuccess: { (homeDataSource) in
+            print("Successfully fetched our json objects")
+            completion(homeDataSource)
+        }) { (err) in
+            print("Failed to fetch json...",err)
+        }
+    }
+    
+    class JSONError: JSONDecodable {
+        required init(json: JSON) throws {
+            print("JSON ERROR")
+        }
+    }
+}
